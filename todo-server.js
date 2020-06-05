ae5f3b32743a93288f82f2ffede6c01124e0f634
lauren <lliebert@uoregon.edu>
2020-06-04 17:32:25 -0700

    Finished To-Do App: Requirement 3

--- todo-server.js
+++ todo-server.js
@@ -11,8 +11,8 @@ const port = 8888,
   dbURL = "mongodb://127.0.0.1:27017",
   MongoClient = require("mongodb").MongoClient,
   client = new MongoClient(dbURL),
-  dbName = "commentsDB",
-  collName = "comments";
+  dbName = "todoDB",
+  collName = "todos";
 
 let db,
   col,
@@ -51,8 +51,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// endpoint to Get all comments
-app.get("/comments", (req, res) => {
+// endpoint to Get all todos
+app.get("/todos", (req, res) => {
   db.collection(collName)
     .find({})
     .toArray(function(err, result) {
@@ -60,14 +60,14 @@ app.get("/comments", (req, res) => {
       //console.log(result);
       res.status(200).send({
         success: "true",
-        message: "comments retrieved successfully",
-        comments: result
+        message: "todos retrieved successfully",
+        todos: result
       });
     });
 });
 
-//endpoint to get a single comment
-app.get("/getcomment/:data", (req, res, next) => {
+//endpoint to get a single todo
+app.get("/gettodo/:data", (req, res, next) => {
   const data = req.params.data;
 
   db.collection(collName).findOne(
@@ -79,34 +79,34 @@ app.get("/getcomment/:data", (req, res, next) => {
       console.log(result);
       res.status(200).send({
         status_code: 200,
-        message: result ? result : "comment not found"
+        message: result ? result : "todo not found"
       });
     }
   );
 });
 
-//Endpoint to add a comment
-app.post("/addcomment", (req, res) => {
+//Endpoint to add a todo
+app.post("/addtodo", (req, res) => {
   // Insert a single document
-  let comment = {
+  let todo = {
     data: req.body.data
   };
 
   db.collection(collName)
-    .insertOne(comment)
+    .insertOne(todo)
     .then(result => {
       console.log(`record inserted ${result}`);
       return res.status(201).send({
         status_code: 200,
-        message: "Comment added successfully",
-        comment
+        message: "todo added successfully",
+        todo
       });
     })
     .catch(error => console.error(error));
 });
 
-//Endpoint to Delete a single comment
-app.post("/deletecomment/:data", (req, res) => {
+//Endpoint to Delete a single todo
+app.post("/deletetodo/:data", (req, res) => {
   const data = req.params.data;
   console.log(data);
   db.collection(collName).deleteOne(
@@ -122,12 +122,12 @@ app.post("/deletecomment/:data", (req, res) => {
 
   return res.status(200).send({
     status_code: 200,
-    message: "comment deleted"
+    message: "todo deleted"
   });
 });
 
-//Endpoint to Delete all tel numbers
-app.delete("/deletenums", (req, res) => {
+//Endpoint to Delete all todos
+app.delete("/deletetodos", (req, res) => {
   db.collection(collName).deleteMany({}, function(err, obj) {
     if (err) throw err;
     //number of documents deleted
